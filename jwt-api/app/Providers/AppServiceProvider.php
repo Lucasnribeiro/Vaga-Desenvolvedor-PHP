d860703a40bbedb48c1187f47b57999db6de7e6b
lucasmatheusmovin <29964166+Lucasnribeiro@users.noreply.github.com>
2022-06-02 11:45:33 -0300

    boot for the client observer

--- jwt-api/app/Providers/AppServiceProvider.php
+++ jwt-api/app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Client;
+use App\Observers\ClientObserver;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Client::observe(ClientObserver::class);
     }
 }
